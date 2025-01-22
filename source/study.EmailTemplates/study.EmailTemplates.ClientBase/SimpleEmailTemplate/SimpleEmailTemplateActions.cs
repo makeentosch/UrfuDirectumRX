efaa53b63a2887f6eb6e3c4f90c241efa2f70744
makeentosch <artem.irk45@gmail.com>
2025-01-22 21:34:42 +0500

    fix: removed html tags in template preview window

--- source/study.EmailTemplates/study.EmailTemplates.ClientBase/SimpleEmailTemplate/SimpleEmailTemplateActions.cs
+++ source/study.EmailTemplates/study.EmailTemplates.ClientBase/SimpleEmailTemplate/SimpleEmailTemplateActions.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using Sungero.Core;
 using Sungero.CoreEntities;
 using study.EmailTemplates.SimpleEmailTemplate;
@@ -10,70 +11,87 @@ using Sungero.Domain.Shared;
 
 namespace study.EmailTemplates.Client
 {
-  partial class SimpleEmailTemplateActions
-  {
-    public virtual void GenerateTemplateAction(Sungero.Domain.Client.ExecuteActionArgs e)
-    {    
-        var variables = GetVars(_obj.DocumentRef);
-        var generatedTemplate = Functions.SimpleEmailTemplate.GenerateTemplate(_obj, variables);
-        
-        var temp = generatedTemplate[0];
-        var email = generatedTemplate[1];
-        
-        var taskDialog = Dialogs.CreateTaskDialog("Сгенерированный шаблон", temp);
-        var emailButton = taskDialog.Buttons.AddCustom("Отправить по эл. почте");
-        
-        if (taskDialog.Show() == emailButton)
-        {
-          Functions.SimpleEmailTemplate.Remote.SendEmail(_obj, temp, email);
-          Dialogs.ShowMessage("Письмо успешно отправлено");
-        }
-        
-        taskDialog.Buttons.AddOk();
-        taskDialog.Show();  
-    }
-
-    public virtual bool CanGenerateTemplateAction(Sungero.Domain.Client.CanExecuteActionArgs e)
+    partial class SimpleEmailTemplateActions
     {
-      return true;
-    }
-    
-    public static Dictionary<string, string> GetVars(Sungero.Docflow.IOfficialDocument document)
-    {        
-        var properties = new Dictionary<string, string>();
-        ProcessProperties(document, properties, 0);
-        return properties;
-    }
-
-    private static void ProcessProperties(object entity, Dictionary<string, string> properties, int count, string prefix = "")
-    {
-        if (entity == null || count == 5)
-            return;
-
-        count++;     
-        var type = entity.GetType();
-        foreach (var property in type.GetProperties())
+        public virtual void GenerateTemplateAction(Sungero.Domain.Client.ExecuteActionArgs e)
         {
-            try
+            var variables = GetVars(_obj.DocumentRef);
+            var generatedTemplate = Functions.SimpleEmailTemplate.GenerateTemplate(_obj, variables);
+
+            var temp = generatedTemplate[0];
+            var noHtmlTemp = RemoveHtmlTags(generatedTemplate[0]);
+            var email = generatedTemplate[1];
+
+            var taskDialog = Dialogs.CreateTaskDialog("Сгенерированный шаблон", noHtmlTemp);
+            var emailButton = taskDialog.Buttons.AddCustom("Отправить по эл. почте");
+
+            if (taskDialog.Show() == emailButton)
             {
-                var value = property.GetValue(entity);
-                var propertyName = string.IsNullOrEmpty(prefix) ? property.Name : $"{prefix} -> {property.Name}";
+                Functions.SimpleEmailTemplate.Remote.SendEmail(_obj, temp, email);
+                Dialogs.ShowMessage("Письмо успешно отправлено");
+            }
 
-                if (value is Sungero.Domain.Shared.IEntity linkedEntity)
+            taskDialog.Buttons.AddOk();
+            taskDialog.Show();
+        }
+
+        public virtual bool CanGenerateTemplateAction(Sungero.Domain.Client.CanExecuteActionArgs e)
+        {
+            return true;
+        }
+
+        public static Dictionary<string, string> GetVars(Sungero.Docflow.IOfficialDocument document)
+        {
+            var properties = new Dictionary<string, string>();
+            ProcessProperties(document, properties, 0);
+            return properties;
+        }
+
+        private static void ProcessProperties(object entity, Dictionary<string, string> properties, int count, string prefix = "")
+        {
+            if (entity == null || count == 5)
+                return;
+
+            count++;
+            var type = entity.GetType();
+            foreach (var property in type.GetProperties())
+            {
+                try
                 {
-                    ProcessProperties(linkedEntity, properties, count, propertyName);
+                    var value = property.GetValue(entity);
+                    var propertyName = string.IsNullOrEmpty(prefix) ? property.Name : $"{prefix} -> {property.Name}";
+
+                    if (value is Sungero.Domain.Shared.IEntity linkedEntity)
+                    {
+                        ProcessProperties(linkedEntity, properties, count, propertyName);
+                    }
+                    else
+                    {
+                        properties[propertyName] = value?.ToString() ?? "Пусто";
+                    }
                 }
-                else
+                catch (Exception ex)
                 {
-                    properties[propertyName] = value?.ToString() ?? "Пусто";
+                    var propertyName = string.IsNullOrEmpty(prefix) ? property.Name : $"{prefix} -> {property.Name}";
+                    properties[propertyName] = "Ошибка";
                 }
             }
-            catch (Exception ex)
-            {
-                var propertyName = string.IsNullOrEmpty(prefix) ? property.Name : $"{prefix} -> {property.Name}";
-                properties[propertyName] = "Ошибка";
-            }
+        }
+
+        private string RemoveHtmlTags(string text)
+        {
+            var result = Regex.Replace(text, "<br\\s*/?>", "\n");
+            result = Regex.Replace(result, "<p.*?>", "\n");
+            result = Regex.Replace(result, "</p>", "\n");
+            result = Regex.Replace(result, "<.*?>", "");
+            result = result.Replace("&nbsp;", " ")
+                .Replace("&lt;", "<")
+                .Replace("&gt;", ">")
+                .Replace("&amp;", "&");
+
+            result = Regex.Replace(result, @"\s{2,}", "\n").Trim();
+
+            return result;
         }
     }
-  }
 }
--- source/study.TemplateSolution/study.TemplateSolution.ClientBase/OfficialDocument/OfficialDocumentActions.cs
+++ source/study.TemplateSolution/study.TemplateSolution.ClientBase/OfficialDocument/OfficialDocumentActions.cs
@@ -1,44 +1,63 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using Sungero.Core;
 using Sungero.CoreEntities;
 using study.TemplateSolution.OfficialDocument;
 
 namespace study.TemplateSolution.Client
 {
-  partial class OfficialDocumentActions
-  {
-    public virtual void CreateTemplatestudy(Sungero.Domain.Client.ExecuteActionArgs e)
+    partial class OfficialDocumentActions
     {
-      var d = Dialogs.CreateInputDialog("Выберите шаблон");
-      var template = d.AddSelect("Шаблон", true, EmailTemplates.SimpleEmailTemplates.Null);
-      
-      if (d.Show() == DialogButtons.Ok)
-      {      
-        var variables = Functions.OfficialDocument.GetVars(_obj);
-        var result = study.EmailTemplates.PublicFunctions.SimpleEmailTemplate.GenerateTemplate(template.Value, variables);
-        
-        var temp = result[0];
-        var email = result[1];
-        
-        var taskDialog = Dialogs.CreateTaskDialog("Сгенерированный шаблон", temp);
-        var emailButton = taskDialog.Buttons.AddCustom("Отправить по эл. почте");
-        
-        if (taskDialog.Show() == emailButton)
+        public virtual void CreateTemplatestudy(Sungero.Domain.Client.ExecuteActionArgs e)
         {
-          Functions.OfficialDocument.Remote.SendEmail(_obj, temp, email);
-          Dialogs.ShowMessage("Письмо успешно отправлено");
+            var d = Dialogs.CreateInputDialog("Выберите шаблон");
+            var template = d.AddSelect("Шаблон", true, EmailTemplates.SimpleEmailTemplates.Null);
+
+            if (d.Show() == DialogButtons.Ok)
+            {
+                var variables = Functions.OfficialDocument.GetVars(_obj);
+                var result = study.EmailTemplates.PublicFunctions.SimpleEmailTemplate.GenerateTemplate(template.Value, variables);
+
+                var temp = result[0];
+                var noHtmlTemp = RemoveHtmlTags(result[0]);
+                var email = result[1];
+
+                var taskDialog = Dialogs.CreateTaskDialog("Сгенерированный шаблон", noHtmlTemp);
+                var emailButton = taskDialog.Buttons.AddCustom("Отправить по эл. почте");
+
+                if (taskDialog.Show() == emailButton)
+                {
+                    Functions.OfficialDocument.Remote.SendEmail(_obj, temp, email);
+                    Dialogs.ShowMessage("Письмо успешно отправлено");
+                }
+
+                taskDialog.Buttons.AddOk();
+                taskDialog.Show();
+            }
         }
 
-        taskDialog.Buttons.AddOk();
-        taskDialog.Show();        
-      }
-    }
+        private string RemoveHtmlTags(string text)
+        {
+            var result = Regex.Replace(text, "<br\\s*/?>", "\n");
+            result = Regex.Replace(result, "<p.*?>", "\n");
+            result = Regex.Replace(result, "</p>", "\n");
+            result = Regex.Replace(result, "<.*?>", "");
+            result = result.Replace("&nbsp;", " ")
+                .Replace("&lt;", "<")
+                .Replace("&gt;", ">")
+                .Replace("&amp;", "&");
 
-    public virtual bool CanCreateTemplatestudy(Sungero.Domain.Client.CanExecuteActionArgs e)
-    {
-      return true;
+            result = Regex.Replace(result, @"\s{2,}", "\n").Trim();
+
+            return result;
+        }
+
+
+        public virtual bool CanCreateTemplatestudy(Sungero.Domain.Client.CanExecuteActionArgs e)
+        {
+            return true;
+        }
     }
-  }
 }
